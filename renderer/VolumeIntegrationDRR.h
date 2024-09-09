9eb346cdb9656b8470c5a47b113376601fc234a5
Matthias Hellmann <m.hellmann@me.com>
2024-09-09 17:51:09 +0200

    copy raycaster changes to DRR: dt_scale

--- renderer/VolumeIntegrationDRR.h
+++ renderer/VolumeIntegrationDRR.h
@@ -17,19 +17,35 @@ inline float rayMarchVolumeDRR(ScreenSample &ss,
   constexpr float min_contribution = 0.6f;
   constexpr float min_intensity = 0.5f;
 
-  float dt = vol.field.baseDT;
+  float dt = vol.unitDistance;
   auto boxHit = intersect(ray, vol.bounds);
+
+  const auto &sf = vol.field;
+
+  ray.tmin = max(ray.tmin, boxHit.tnear);
+  ray.tmax = min(ray.tmax, boxHit.tfar);
+
+  // transform ray to voxel space
+  ray.ori = sf.pointToVoxelSpace(ray.ori);
+  ray.dir = sf.vectorToVoxelSpace(ray.dir);
+
+  const float dt_scale = length(ray.dir);
+  ray.dir = normalize(ray.dir);
+
+  ray.tmin = ray.tmin * dt_scale;
+  ray.tmax = ray.tmax * dt_scale;
+  dt = dt * dt_scale;
 
   // render
   float v_max = 0.f;
   float t_at_v_max = 0.f;
   float lac_accumulated = 0.f;
   size_t steps = 0;
-  float t=boxHit.tnear;
-  for (; t<boxHit.tfar; t+=dt) {
-    float3 P = ray.ori+ray.dir*t;
+  float t=ray.tmin;
+  for (; t<ray.tmax; t+=dt) {
+    float3 P = ray.ori + ray.dir * t;
     float v = 0.f;
-    if (sampleField(vol.field,P,v)) {
+    if (sampleField(sf, P, v)) {
       lac_accumulated += v;
       if (v > v_max) {
         v_max = v;
@@ -39,7 +55,7 @@ inline float rayMarchVolumeDRR(ScreenSample &ss,
     }
   }
   auto lac_averaged = lac_accumulated / steps;
-  auto dist_cm = (steps * dt) / 10.f; //TODO assuming dt is in [mm]
+  auto dist_cm = (steps * dt / dt_scale) / 10.f; //TODO assuming dt is in [mm]
   auto remaining = pow(photon_energy, -dist_cm * lac_averaged);
   color = float3(1.f - remaining);
   alpha = 1.f;
@@ -47,25 +63,25 @@ inline float rayMarchVolumeDRR(ScreenSample &ss,
   // get depth
   if (color.x < min_intensity)
     return 0.f;
-  const float start = max(t_at_v_max - depth_accum_dist_mm / 2.f, boxHit.tnear);
-  const float end   = min(t_at_v_max + depth_accum_dist_mm / 2.f, boxHit.tfar);
+  const float start = max(t_at_v_max - depth_accum_dist_mm * dt_scale / 2.f, boxHit.tnear);
+  const float end   = min(t_at_v_max + depth_accum_dist_mm * dt_scale / 2.f, boxHit.tfar);
   auto t2 = start;
   float section_lac_accumulated = 0.f;
   size_t section_steps = 0;
   for (; t2<end; t2+=dt) {
     float3 P = ray.ori+ray.dir*t2;
     float v = 0.f;
-    if (sampleField(vol.field,P,v)) {
+    if (sampleField(sf, P, v)) {
       section_lac_accumulated += v;
       ++section_steps;
     }
   }
   auto section_lac_averaged = section_lac_accumulated / section_steps;
-  auto section_dist_cm = (section_steps * dt) / 10.f; //TODO assuming dt is in [mm]
+  auto section_dist_cm = (section_steps * dt / dt_scale) / 10.f; //TODO assuming dt is in [mm]
   auto section_remaining = pow(photon_energy, -section_dist_cm * section_lac_averaged);
   auto contribution = (1.f - section_remaining) / (1.f - remaining);
   if (contribution >= min_contribution)
-    return t_at_v_max;
+    return t_at_v_max / dt_scale;
   return 0.f;
 }
 
