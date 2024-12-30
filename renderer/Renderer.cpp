dc0323f540cfc8f74bcd2f949cba15c150c09389
Matthias Hellmann <hellmann@uni-koeln.de>
2024-12-30 12:54:38 +0100

    make drr default

--- renderer/Renderer.cpp
+++ renderer/Renderer.cpp
@@ -88,11 +88,11 @@ void Renderer::commit()
 
 Renderer *Renderer::createInstance(std::string_view subtype, VisionarayGlobalState *s)
 {
-  if (subtype == "direct_light" || subtype == "default")
+  if (subtype == "direct_light")
     return new DirectLight(s);
   else if (subtype == "raycast")
     return new Raycast(s);
-  else if (subtype == "drr")
+  else if (subtype == "drr" || subtype == "default")
     return new DRR(s);
   else
     return {};
